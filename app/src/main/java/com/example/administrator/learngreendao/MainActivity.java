fd84b49525c1f6d4d3fc8835f3ecfa17cfd14526
shuxing <caishuxing@linggan.com>
2015-11-25 11:09:32 +0800

    添加方法

--- app/src/main/java/com/example/administrator/learngreendao/MainActivity.java
+++ app/src/main/java/com/example/administrator/learngreendao/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.administrator.learngreendao;
 
+import android.app.Activity;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import junit.framework.Assert;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -15,7 +19,7 @@ import de.greenrobot.dao.query.DeleteQuery;
 import de.greenrobot.dao.query.Query;
 import de.greenrobot.dao.query.QueryBuilder;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     SQLiteDatabase db;
     DaoMaster daoMaster;
     DaoSession daoSession;
@@ -26,12 +30,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initSql();
-        insertEntity();
-     //  delete();
-        print(query());
+        insertAllEntity();
+//        delete();
+//        querySql();
+//        query("comment2");
+//        queryRaw();
+//        limit();
+//        order();
+//        offsetAndLimit();
+       // update();
+        load();
     }
 
-    private void initSql() {
+    public void initSql() {
         DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "test-db", null);
         db = helper.getWritableDatabase();
         daoMaster = new DaoMaster(db);
@@ -47,7 +58,13 @@ public class MainActivity extends AppCompatActivity {
         return note;
     }
 
-    public void insertEntity() {
+    public Note creatNoteEntity(long id, String comment, String text) {
+        Note note = creatNoteEntity(comment, text);
+        note.setId(id);
+        return note;
+    }
+
+    public void insertAllEntity() {
         List<Note> noteList = new ArrayList<>();
         for (int i = 0; i < 20; i++) {
             noteList.add(creatNoteEntity("commnet" + i, "text" + i));
@@ -55,18 +72,31 @@ public class MainActivity extends AppCompatActivity {
         noteDao.insertInTx(noteList);
     }
 
+    public void update() {
+        long id= insertEntity();
+        Note note = creatNoteEntity(id, "888", "888");
+        noteDao.update(note);
+        querySql();
+    }
+
+    public long insertEntity() {
+       Note note=creatNoteEntity("999", "999");
+       return noteDao.insert(note);
+    }
     public void order() {
         QueryBuilder<Note> queryBuilder = noteDao.queryBuilder();
         queryBuilder.orderAsc(NoteDao.Properties.Comment);
         queryBuilder.orderDesc(NoteDao.Properties.Text);
-        queryBuilder.list();
+        List<Note> noteList = queryBuilder.list();//queryBuilder.list();等价于 Query<Note> query = queryBuilder.build();+query.list()
+        print(noteList);
     }
 
     public void offsetAndLimit() {
         QueryBuilder<Note> queryBuilder = noteDao.queryBuilder();
         queryBuilder.limit(3);
         queryBuilder.offset(2);
-        queryBuilder.list();
+        List<Note> noteList = queryBuilder.list();//queryBuilder.list();等价于 Query<Note> query = queryBuilder.build();+query.list()
+        print(noteList);
     }
 
     public void listLazy() {
@@ -74,23 +104,40 @@ public class MainActivity extends AppCompatActivity {
         queryBuilder.listLazy();
     }
 
-    public void limit() {
+    public List<Note> limit() {
         QueryBuilder<Note> queryBuilder = noteDao.queryBuilder();
         queryBuilder.limit(3);
-        queryBuilder.list();
+        return queryBuilder.list();
     }
 
-    public List<Note> queryRaw() {
+    public void queryRaw() {
         String sql = "WHERE " + NoteDao.Properties.Text.columnName + "=?";
         Query<Note> query = noteDao.queryRawCreate(sql, "text2");
-        return query.list();
+        List<Note> noteList = query.list();//queryBuilder.list();等价于 Query<Note> query = queryBuilder.build();+query.list()
+        print(noteList);
     }
 
-    public List<Note> query() {
+    public void querySql() {
+        String orderBy = NoteDao.Properties.Text.columnName + " COLLATE LOCALIZED ASC";
+        Cursor cursor = db.query(noteDao.getTablename(), noteDao.getAllColumns(), null, null, null, null, orderBy);
+        List<Note> noteList = new ArrayList<>();
+        Note note = null;
+        while (cursor.moveToNext()) {
+            Long id = cursor.getLong(cursor.getColumnIndex(NoteDao.Properties.Id.columnName));
+            String text = cursor.getString(cursor.getColumnIndex(NoteDao.Properties.Text.columnName));
+            String comment = cursor.getString(cursor.getColumnIndex(NoteDao.Properties.Comment.columnName));
+            note = creatNoteEntity(id, text, comment);
+            noteList.add(note);
+        }
+        print(noteList);
+    }
+
+    public void query(String comment) {
         QueryBuilder<Note> queryBuilder = noteDao.queryBuilder();
-        queryBuilder.where(NoteDao.Properties.Comment.eq("commnet2"));
+        queryBuilder.where(NoteDao.Properties.Comment.eq(comment));
         Query<Note> query = queryBuilder.build();
-        return query.list();//或queryBuilder.list();
+        List<Note> noteList = query.list();//queryBuilder.list();等价于 Query<Note> query = queryBuilder.build();+query.list()
+        print(noteList);
     }
 
     public void delete() {
@@ -98,32 +145,15 @@ public class MainActivity extends AppCompatActivity {
         queryBuilder.where(NoteDao.Properties.Comment.eq("commnet2"));
         DeleteQuery<Note> query = queryBuilder.buildDelete();
         query.executeDeleteWithoutDetachingEntities();
+        querySql();
+    }
+
+    public void load(){
+        List<Note> noteList = noteDao.loadAll();//noteDao.load(1l);
+        print(noteList);
     }
 
     public void print(List<Note> noteList) {
         Log.d("note:-->", noteList.toString());
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
